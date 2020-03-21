851e985e91b7b23f1708654f69d2f800a76d8d78
aleksi_ttu <aleksi@ttu.ee>
2020-03-21 19:18:27 +0200

    hw3 - leg1

--- HomeProject/DAL.Base/DomainEntity.cs
+++ HomeProject/DAL.Base/DomainEntity.cs
@@ -3,17 +3,13 @@ using Contracts.DAL.Base;
 
 namespace DAL.Base
 {
-    
-    
-    
-    public abstract class DomainEntity: DomainBaseEntity, IDomainEntityMetadata
+    public abstract class DomainEntity : IDomainEntity
     {
+        public Guid Id { get; set; }
         public string CreatedBy { get; set; }
         public DateTime CreatedAt { get; set; }
-        
         public string ChangedBy { get; set; }
         public DateTime ChangedAt { get; set; }
-        
         public string DeletedBy { get; set; }
         public DateTime? DeletedAt { get; set; }
     }
