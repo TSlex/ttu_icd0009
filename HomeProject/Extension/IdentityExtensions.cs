a470e1d902bb1b4629125eddc17b53baa40cc0b1
aleksi_ttu <aleksi@ttu.ee>
2020-04-02 16:27:31 +0300

    jwt generation

--- HomeProject/Extension/IdentityExtensions.cs
+++ HomeProject/Extension/IdentityExtensions.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Extension
 {
@@ -9,6 +13,24 @@ namespace Extension
         public static string UserId(this ClaimsPrincipal user)
         {
             return user.Claims.Single(claim => claim.Type == ClaimTypes.NameIdentifier).Value;
+        }
+
+        public static string GenerateJWT(IEnumerable<Claim> claims, string signingKey, string issuer, int expiresInDays)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey));
+            var singingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+            var expires = DateTime.Now.AddDays(expiresInDays);
+            
+            var token = new JwtSecurityToken(
+                issuer,
+                issuer,
+                claims,
+                null,
+                expires,
+                singingCredentials
+            );
+            
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
