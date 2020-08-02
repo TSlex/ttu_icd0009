e724b1e1f2dbd588c53bf7c5bf3acaf978d00fb9
aleksi_ttu <aleksi@ttu.ee>
2020-08-02 19:52:12 +0300

    admin panel refactoring

---
+++ HomeProject/WebApp/Models/ImageMiniature.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApp.Models
+{
+    /// <summary>
+    /// Partial view setting holder
+    /// </summary>
+    public class ImageMiniature
+    {
+        /// <summary>
+        /// image miniature styles
+        /// </summary>
+        public string Styles { get; set; } = default!;
+    }
+}
