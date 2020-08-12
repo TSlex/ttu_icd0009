79e726e6ca0d4a4784203529978d902f2afe302e
aleksi_ttu <aleksi@ttu.ee>
2020-08-12 09:20:47 +0300

    homeworks create, edit, delete

--- Exam2019s/Exam/WebApp/Controllers/HomeWorksController.cs
+++ Exam2019s/Exam/WebApp/Controllers/HomeWorksController.cs
@@ -19,14 +19,6 @@ namespace WebApp.Controllers
             _context = context;
         }
 
-        // GET: HomeWorks
-        public async Task<IActionResult> Index()
-        {
-            var applicationDbContext = _context.HomeWorks.Include(h => h.Subject);
-            return View(await applicationDbContext.ToListAsync());
-        }
-
-        // GET: HomeWorks/Details/5
         public async Task<IActionResult> Details(Guid? id)
         {
             if (id == null)
@@ -37,6 +29,7 @@ namespace WebApp.Controllers
             var homeWork = await _context.HomeWorks
                 .Include(h => h.Subject)
                 .FirstOrDefaultAsync(m => m.Id == id);
+            
             if (homeWork == null)
             {
                 return NotFound();
@@ -45,86 +38,33 @@ namespace WebApp.Controllers
             return View(homeWork);
         }
 
-        // GET: HomeWorks/Create
-        public IActionResult Create()
+        public async Task<IActionResult> Create(Guid subjectId)
         {
-            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "SubjectCode");
-            return View();
+            var subject = await _context.Subjects.FirstOrDefaultAsync(s => s.Id == subjectId);
+            
+            return View(new HomeWork
+            {
+                Subject = subject
+            });
         }
 
-        // POST: HomeWorks/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Title,Description,Deadline,SubjectId,Id,CreatedBy,CreatedAt,ChangedBy,ChangedAt,DeletedBy,DeletedAt")] HomeWork homeWork)
+        public async Task<IActionResult> Create(HomeWork homeWork)
         {
             if (ModelState.IsValid)
             {
                 homeWork.Id = Guid.NewGuid();
+
                 _context.Add(homeWork);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Details", "Subjects", new {id = homeWork.SubjectId});
             }
-            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "SubjectCode", homeWork.SubjectId);
+
             return View(homeWork);
         }
 
-        // GET: HomeWorks/Edit/5
         public async Task<IActionResult> Edit(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var homeWork = await _context.HomeWorks.FindAsync(id);
-            if (homeWork == null)
-            {
-                return NotFound();
-            }
-            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "SubjectCode", homeWork.SubjectId);
-            return View(homeWork);
-        }
-
-        // POST: HomeWorks/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, [Bind("Title,Description,Deadline,SubjectId,Id,CreatedBy,CreatedAt,ChangedBy,ChangedAt,DeletedBy,DeletedAt")] HomeWork homeWork)
-        {
-            if (id != homeWork.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(homeWork);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!HomeWorkExists(homeWork.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "SubjectCode", homeWork.SubjectId);
-            return View(homeWork);
-        }
-
-        // GET: HomeWorks/Delete/5
-        public async Task<IActionResult> Delete(Guid? id)
         {
             if (id == null)
             {
@@ -134,6 +74,7 @@ namespace WebApp.Controllers
             var homeWork = await _context.HomeWorks
                 .Include(h => h.Subject)
                 .FirstOrDefaultAsync(m => m.Id == id);
+
             if (homeWork == null)
             {
                 return NotFound();
@@ -142,20 +83,37 @@ namespace WebApp.Controllers
             return View(homeWork);
         }
 
-        // POST: HomeWorks/Delete/5
-        [HttpPost, ActionName("Delete")]
+
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        public async Task<IActionResult> Edit(Guid id, HomeWork homeWork)
         {
-            var homeWork = await _context.HomeWorks.FindAsync(id);
-            _context.HomeWorks.Remove(homeWork);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            if (id != homeWork.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(homeWork);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Details", "Subjects", new {id = homeWork.SubjectId});
+            }
+
+            return View(homeWork);
         }
 
-        private bool HomeWorkExists(Guid id)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(Guid id)
         {
-            return _context.HomeWorks.Any(e => e.Id == id);
+            var homeWork = await _context.HomeWorks.FindAsync(id);
+            
+            _context.HomeWorks.Remove(homeWork);
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Details", "Subjects", new {id = homeWork.SubjectId});
         }
     }
 }
